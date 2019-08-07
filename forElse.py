aed13d5d2dad582f9fbabe00a80b3cf008271800
larsmhar <lars.martin.hardeland@gmail.com>
2019-08-07 22:11:10 +0200

    Added else after a for loop

---
+++ forElse.py
@@ -0,0 +1,10 @@
+s = "this is a string"
+n = 0
+
+for char in s:
+    if char == "s":
+        n += 1
+# You can have an else statement after a for loop
+# This will fire when the for loop ends
+else:
+    print(n)
